fe2b69f6b1e8ec5694ac534cb96ad4ac09e6e4af
Rayan Khan <iRayanKhan@icloud.com>
2024-01-18 22:47:39 -0600

    Initial Commit

---
+++ APNS Demo/APNS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  APNS_DemoApp.swift
+//  APNS Demo
+//
+//  Created by Rayan Khan on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct APNS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APNS Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APNS Demo
+//
+//  Created by Rayan Khan on 1/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
