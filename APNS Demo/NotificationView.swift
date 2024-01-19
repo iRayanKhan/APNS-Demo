52e7fa861e1806ebd6758ff56426d9fe24b6e8b6
Rayan Khan <iRayanKhan@icloud.com>
2024-01-18 22:49:16 -0600

    - Add NotificationView file

---
+++ APNS Demo/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  APNS Demo
+//
+//  Created by Rayan Khan on 1/18/24.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
